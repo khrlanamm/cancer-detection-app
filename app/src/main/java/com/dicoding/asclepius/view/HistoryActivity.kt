7bda8cdd3b6385b096794faef1ecdfa2b261356c
khrlanamm <anam160404@gmail.com>
2024-11-19 03:35:40 +0700

    DONE Analyze and History

--- app/src/main/java/com/dicoding/asclepius/view/HistoryActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/HistoryActivity.kt
@@ -2,26 +2,33 @@ package com.dicoding.asclepius.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.data.local.entity.AnalyzeEntity
+import com.dicoding.asclepius.data.local.room.AnalyzeDatabase
 import com.dicoding.asclepius.helper.AnalyzeAdapter
+import com.dicoding.asclepius.view.ResultActivity.Companion.REQUEST_HISTORY_UPDATE
 import com.example.bottomnavsampleapp.startActivityWithNavBarSharedTransition
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class HistoryActivity : AppCompatActivity() {
+class HistoryActivity : AppCompatActivity(), AnalyzeAdapter.OnDeleteClickListener {
     private lateinit var analyzeRecyclerView: RecyclerView
     private lateinit var analyzeAdapter: AnalyzeAdapter
     private var analyzeList: MutableList<AnalyzeEntity> = mutableListOf()
     private lateinit var tvNotFound: TextView
 
-    companion object{
+    companion object {
         const val TAG = "analyzedata"
     }
 
@@ -59,7 +66,78 @@ class HistoryActivity : AppCompatActivity() {
                 }
             }
         }
+        analyzeRecyclerView = findViewById(R.id.rvHistory)
+        tvNotFound = findViewById(R.id.tvNotFound)
 
+        analyzeAdapter = AnalyzeAdapter(analyzeList)
+        analyzeAdapter.setOnDeleteClickListener(this)
+        analyzeRecyclerView.adapter = analyzeAdapter
+        analyzeRecyclerView.layoutManager = LinearLayoutManager(this)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            loadAnalyzeFromDatabase()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_HISTORY_UPDATE && resultCode == RESULT_OK) {
+            GlobalScope.launch(Dispatchers.Main) {
+                loadAnalyzeFromDatabase()
+            }
+        }
+    }
+
+    private fun loadAnalyzeFromDatabase() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val predictions =
+                AnalyzeDatabase.getDatabase(this@HistoryActivity).analyzeDao().getAllPredictions()
+            Log.d(TAG, "Number of predictions: ${predictions.size}")
+            analyzeList.clear()
+            analyzeList.addAll(predictions)
+            analyzeAdapter.notifyDataSetChanged()
+            showOrHideNoHistoryText()
+        }
+    }
+
+    private fun showOrHideNoHistoryText() {
+        if (analyzeList.isEmpty()) {
+            tvNotFound.visibility = View.VISIBLE
+            analyzeRecyclerView.visibility = View.GONE
+        } else {
+            tvNotFound.visibility = View.GONE
+            analyzeRecyclerView.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onDeleteClick(position: Int) {
+        val prediction = analyzeList[position]
+        if (prediction.result.isNotEmpty()) {
+            GlobalScope.launch(Dispatchers.IO) {
+                AnalyzeDatabase.getDatabase(this@HistoryActivity).analyzeDao()
+                    .deletePrediction(prediction)
+            }
+            analyzeList.removeAt(position)
+            analyzeAdapter.notifyDataSetChanged()
+            showOrHideNoHistoryText()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
